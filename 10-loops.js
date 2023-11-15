04c872415fb718270b40b251fe721069a1481b0b
themrsgod <themrsgod@gmail.com>
2023-11-15 17:03:23 +0000

    corrections

--- 10-loops.js
+++
@@ -1,9 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const newArray = [];
-  // eslint-disable-next-line no-restricted-syntax
-  for (const value of array) {
-    newArray.push(appendString + value);
-  }
-
-  return newArray;
-}
--- 12-createReportObject.js
+++
@@ -1,9 +0,0 @@
-export default function createReportObject(employeesList) {
-  return {
-    allEmployees: employeesList,
-    // eslint-disable-next-line no-shadow
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length;
-    },
-  };
-}
